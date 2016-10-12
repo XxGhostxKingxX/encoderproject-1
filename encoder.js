7c314043d6bb77c2b0c402c0ba0c50fabacd4803
Jose Amezcua <amezcua.jose29@gmail.com>
2016-10-12 09:18:15 -0700

    started project
    added input for radio buttons

---
+++ encoder.js
@@ -0,0 +1,7 @@
+$("#divname").keydown(function(e) {
+  // code to execute when user presses a key goes here
+});
+
+<input type="radio" name="echo" id="echo" value="echo"> Echo<br>
+<input type="radio" name="caesarcipher" id="cipher" value="cipher"> Caesarr Cipher<br>
+<input type="radio" name="heiroglyphics" id="heiroglyphics" value="heiroglyphics"> Heiroglyphics<br>
