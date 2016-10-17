b06ce16a40df8e7bf6fda35cf4e7d09ce20b375f
Jose Amezcua <amezcua.jose29@gmail.com>
2016-10-17 09:54:49 -0700

    added canvas code

--- encoder.js
+++ encoder.js
@@ -5,3 +5,14 @@ $("#divname").keydown(function(e) {
 <input type="radio" name="echo" id="echo" value="echo"> Echo<br>
 <input type="radio" name="caesarcipher" id="cipher" value="cipher"> Caesarr Cipher<br>
 <input type="radio" name="heiroglyphics" id="heiroglyphics" value="heiroglyphics"> Heiroglyphics<br>
+
+$("input[type='radio']"._____(function() {
+  $("input:checked").prop('checked', ____)
+  $(this).prop('checked', ____);
+});
+
+if ($("input:checked").val() == "echo") {
+  // code here
+}
+
+var messageArray = messageToDecode.split("")
